be7f4c758c5938ddddf6d001f821123ec904df88
vx007 <vasilyev.x@mail.ru>
2022-11-09 20:39:17 +0300

    Homework

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
+import java.util.ArrayList;
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
-
+        ArrayList<Integer> list = new ArrayList<>(20);
+        Random random = new Random();
+        for (int i = 0; i < 20; i++) {
+            list.add(random.nextInt(0, 1000));
+        }
+        System.out.println(list);
+        list.removeIf(i -> i % 2 == 1);
+        System.out.println(list);
     }
 }
